a0c43913d5316ffc5126e67b72bd27dc50688040
MuhammadRiyoUbaidillah <82527896+MuhammadRiyoUbaidillah@users.noreply.github.com>
2021-04-28 15:57:51 +0700

    Create DataUniversitas.java

---
+++ DataUniversitas.java
@@ -0,0 +1,32 @@
+package modul3;
+
+public class DataUniversitas {
+    static String univ;
+    static String NIM;
+    static String Nama;
+
+    public static String getUniv() {
+        return univ;
+    }
+
+    public static void setUniv(String univ) {
+        DataUniversitas.univ = univ;
+    }
+
+    public static String getNIM() {
+        return NIM;
+    }
+
+    public static void setNIM(String NIM) {
+        DataUniversitas.NIM = NIM;
+    }
+
+    public static String getNama() {
+        return Nama;
+    }
+
+    public static void setNama(String Nama) {
+        DataUniversitas.Nama = Nama;
+    }
+ 
+}
